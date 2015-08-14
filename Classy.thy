7b64747a8b75d054475ff399ad7bb99d17113909
Lars Hupel <lars.hupel@mytum.de>
2015-08-14 23:55:39 +0200

    tests

---
+++ Classy.thy
@@ -0,0 +1,7 @@
+theory Classy
+imports Pure
+begin
+
+ML_file "ml_types.ML"
+
+end
---
+++ Test.thy
@@ -0,0 +1,41 @@
+theory Test
+imports Classy
+begin
+
+ML\<open>
+fun check t =
+  let
+    val t' = ML_Types.read_ml_type t |> ML_Types.pretty |> Pretty.string_of |> YXML.content_of
+  in
+    if t <> t' then
+      error t'
+    else
+      ()
+  end
+\<close>
+
+ML\<open>check "'a"\<close>
+ML\<open>check "'a * 'b"\<close>
+ML\<open>check "'a * 'b * 'c"\<close>
+ML\<open>check "('a * 'b) * 'c"\<close>
+ML\<open>check "'a * ('b * 'c)"\<close>
+ML\<open>check "'a * 'b -> 'c"\<close>
+ML\<open>check "'a * 'b -> 'a * 'b"\<close>
+ML\<open>check "('a -> 'b) * ('a -> 'b)"\<close>
+ML\<open>check "('a, 'b) foo"\<close>
+ML\<open>check "'a foo"\<close>
+ML\<open>check "unit foo"\<close>
+ML\<open>check "unit * 'a"\<close>
+ML\<open>check "'a foo * 'b foo bar"\<close>
+ML\<open>check "('a, unit) foo"\<close>
+ML\<open>check "('a * 'b) foo"\<close>
+ML\<open>check "('a -> 'b) foo"\<close>
+ML\<open>check "('a -> 'b, 'c) foo"\<close>
+ML\<open>check "('a -> 'b, 'c * 'd) foo"\<close>
+ML\<open>check "'a foo bar baz"\<close>
+ML\<open>check "('a foo, 'b) bar Long.ident.baz"\<close>
+ML\<open>check "{a: int}"\<close>
+ML\<open>check "{a: int, b: float}"\<close>
+ML\<open>check "{a: int, b: 'a -> 'b}"\<close>
+
+end
