7875cfb5eb6a0e08cb6fb866f57f15afe67d78d5
Lars Hupel <lars.hupel@mytum.de>
2015-08-25 23:08:06 +0200

    bookkeeping for classes and instances

--- Classy.thy
+++ Classy.thy
@@ -1,7 +1,12 @@
 theory Classy
 imports Pure
+keywords
+  "ML.class" :: thy_decl % "ML" and
+  "ML.instance" :: thy_decl % "ML" and
+  "ML.print_classes" :: diag
 begin
 
 ML_file "ml_types.ML"
+ML_file "classy.ML"
 
 end
