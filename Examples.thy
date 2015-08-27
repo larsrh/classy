38f4789de07ea2efae6fd92e4358afbbde6bebc5
Lars Hupel <lars.hupel@mytum.de>
2015-08-27 22:02:02 +0200

    more example

--- Examples.thy
+++ Examples.thy
@@ -4,6 +4,15 @@ begin
 
 ML.print_classes
 
-ML\<open>format @{ML.resolve \<open>string list\<close> :: pretty} ["a", "b"] |> Pretty.writeln\<close>
+ML\<open>format @{ML.resolve \<open>string list\<close> :: pretty} ["a", "b"] |> Pretty.writeln\<close> (* okay *)
+
+context begin
+
+  (* conflicting instance *)
+  ML.instance \<open>Pretty_Class.string\<close> :: pretty
+
+  ML\<open>@{ML.resolve \<open>string\<close> :: pretty}\<close> (* error *)
+
+end
 
 end
