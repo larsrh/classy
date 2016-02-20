891921d6a764d2bbf11952a24670ff539f29c615
Lars Hupel <lars.hupel@mytum.de>
2016-02-20 16:56:44 +0100

    uniform sources across 2015/2016 using multi-isabelle

--- Classy.thy
+++ Classy.thy
@@ -1,10 +1,20 @@
 theory Classy
-imports Pure
+imports Pure "lib/multi-isabelle/Multi_Isabelle"
 keywords
   "ML.class" :: thy_decl % "ML" and
   "ML.instance" :: thy_decl % "ML" and
   "ML.print_classes" :: diag
 begin
+
+ML_cond ("2015") \<open>
+  val sort_by = sort_wrt
+  fun secure_use_text context {line, file, verbose, debug} txt =
+    Secure.use_text context (line, file) verbose txt
+\<close>
+
+ML_cond ("2016") \<open>
+  val secure_use_text = Secure.use_text
+\<close>
 
 ML_file "ml_types.ML"
 ML_file "classy.ML"
