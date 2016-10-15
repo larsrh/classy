bd75356d6286338a0c40b02ce5c0d894899824ff
Lars Hupel <lars.hupel@mytum.de>
2016-10-15 14:30:52 +0200

    Isabelle2016-1-RC0 compatibility

--- Classy.thy
+++ Classy.thy
@@ -1,19 +1,33 @@
 theory Classy
-imports Pure "lib/multi-isabelle/Multi_Isabelle"
+imports "lib/multi-isabelle/Multi_Isabelle"
 keywords
   "ML.class" :: thy_decl % "ML" and
   "ML.instance" :: thy_decl % "ML" and
   "ML.print_classes" :: diag
 begin
 
-ML_cond ("2015") \<open>
-  val sort_by = sort_wrt
-  fun secure_use_text context {line, file, verbose, debug} txt =
-    Secure.use_text context (line, file) verbose txt
-\<close>
-
 ML_cond ("2016") \<open>
   val secure_use_text = Secure.use_text
+  val context_set_thread_data = Context.setmp_thread_data
+  fun ml_env_context return : use_context =
+    {tune_source = #tune_source ML_Env.local_context,
+     name_space = #name_space ML_Env.local_context,
+     str_of_pos = #str_of_pos ML_Env.local_context,
+     print = fn r => return := SOME r,
+     error = #error ML_Env.local_context}
+  val print_int = Markup.print_int
+\<close>
+
+ML_cond ("2016-1-RC0") \<open>
+  val secure_use_text = ML_Compiler0.ML
+  val context_set_thread_data = Context.setmp_generic_context
+  fun ml_env_context return : ML_Compiler0.context =
+    {name_space = #name_space ML_Env.context,
+     print_depth = SOME 1000000,
+     here = #here ML_Env.context,
+     print = fn r => return := SOME r,
+     error = #error ML_Env.context}
+  val print_int = Value.print_int
 \<close>
 
 ML_file "ml_types.ML"
