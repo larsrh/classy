13ea086f125a74059423596d2d32f1f89617beb3
Lars Hupel <lars.hupel@mytum.de>
2016-11-22 11:04:14 +0100

    Isabelle2016-1-RC3 compatibility

--- src/main/isabelle/Classy.thy
+++ src/main/isabelle/Classy.thy
@@ -42,6 +42,18 @@ ML_cond ("2016-1-RC2") \<open>
   val print_int = Value.print_int
 \<close>
 
+ML_cond ("2016-1-RC3") \<open>
+  val secure_use_text = ML_Compiler0.ML
+  val context_set_thread_data = Context.setmp_generic_context
+  fun ml_env_context return : ML_Compiler0.context =
+    {name_space = #name_space ML_Env.context,
+     print_depth = SOME 1000000,
+     here = #here ML_Env.context,
+     print = fn r => return := SOME r,
+     error = #error ML_Env.context}
+  val print_int = Value.print_int
+\<close>
+
 ML_file "ml_types.ML"
 ML_file "classy.ML"
 
