8813f6a05c16adaa6c76a6e11a9f17aa9257a0b6
Lars Hupel <lars.hupel@mytum.de>
2016-12-13 12:30:15 +0100

    Isabelle2016-1 compatibility

--- src/main/isabelle/Classy.thy
+++ src/main/isabelle/Classy.thy
@@ -18,43 +18,7 @@ ML_cond ("2016") \<open>
   val print_int = Markup.print_int
 \<close>
 
-ML_cond ("2016-1-RC1") \<open>
-  val secure_use_text = ML_Compiler0.ML
-  val context_set_thread_data = Context.setmp_generic_context
-  fun ml_env_context return : ML_Compiler0.context =
-    {name_space = #name_space ML_Env.context,
-     print_depth = SOME 1000000,
-     here = #here ML_Env.context,
-     print = fn r => return := SOME r,
-     error = #error ML_Env.context}
-  val print_int = Value.print_int
-\<close>
-
-ML_cond ("2016-1-RC2") \<open>
-  val secure_use_text = ML_Compiler0.ML
-  val context_set_thread_data = Context.setmp_generic_context
-  fun ml_env_context return : ML_Compiler0.context =
-    {name_space = #name_space ML_Env.context,
-     print_depth = SOME 1000000,
-     here = #here ML_Env.context,
-     print = fn r => return := SOME r,
-     error = #error ML_Env.context}
-  val print_int = Value.print_int
-\<close>
-
-ML_cond ("2016-1-RC3") \<open>
-  val secure_use_text = ML_Compiler0.ML
-  val context_set_thread_data = Context.setmp_generic_context
-  fun ml_env_context return : ML_Compiler0.context =
-    {name_space = #name_space ML_Env.context,
-     print_depth = SOME 1000000,
-     here = #here ML_Env.context,
-     print = fn r => return := SOME r,
-     error = #error ML_Env.context}
-  val print_int = Value.print_int
-\<close>
-
-ML_cond ("2016-1-RC4") \<open>
+ML_cond ("2016-1") \<open>
   val secure_use_text = ML_Compiler0.ML
   val context_set_thread_data = Context.setmp_generic_context
   fun ml_env_context return : ML_Compiler0.context =
