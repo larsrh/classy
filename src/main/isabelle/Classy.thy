21ef0ca4d68d5af407d383d90765bcf0193137ad
Lars Hupel <lars.hupel@mytum.de>
2018-07-03 12:29:24 +0200

    really drop Isabelle2016 support

--- src/main/isabelle/Classy.thy
+++ src/main/isabelle/Classy.thy
@@ -6,31 +6,8 @@ keywords
   "ML.print_classes" :: diag
 begin
 
-ML_cond ("2016") \<open>
-  val secure_use_text = Secure.use_text
-  val context_set_thread_data = Context.setmp_thread_data
-  fun ml_env_context return : use_context =
-    {tune_source = #tune_source ML_Env.local_context,
-     name_space = #name_space ML_Env.local_context,
-     str_of_pos = #str_of_pos ML_Env.local_context,
-     print = fn r => return := SOME r,
-     error = #error ML_Env.local_context}
-  val print_int = Markup.print_int
-\<close>
-
-ML_cond ("2016-1") \<open>
-  val secure_use_text = ML_Compiler0.ML
-  val context_set_thread_data = Context.setmp_generic_context
-  fun ml_env_context return : ML_Compiler0.context =
-    {name_space = #name_space ML_Env.context,
-     print_depth = SOME 1000000,
-     here = #here ML_Env.context,
-     print = fn r => return := SOME r,
-     error = #error ML_Env.context}
-  val print_int = Value.print_int
-\<close>
-
-ML_cond ("2017", "2018-RC0") \<open>
+(* FIXME unfold in ML files *)
+ML\<open>
   val secure_use_text = ML_Compiler0.ML
   val context_set_thread_data = Context.setmp_generic_context
   fun ml_env_context return : ML_Compiler0.context =
